0e283ed7b3a9763830e994fed81b17c2e07dcf0a
Constantin Müller <info@mueller-constantin.de>
2025-04-15 20:08:21 +0200

    feat: add locate position button to map

--- src/components/organisms/DynamicMap.tsx
+++ src/components/organisms/DynamicMap.tsx
@@ -1,8 +1,32 @@
 import React, { useEffect } from "react";
 import Leaflet from "leaflet";
-import { MapContainer, MapContainerProps } from "react-leaflet";
+import { MapContainer, MapContainerProps, useMap } from "react-leaflet";
+import { LocateControl } from "leaflet.locatecontrol";
 
 import "leaflet/dist/leaflet.css";
+import "leaflet.locatecontrol/dist/L.Control.Locate.min.css";
+
+interface MapPluginsProps {}
+
+function MapPlugins(props: MapPluginsProps) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (map) {
+      const locateControl = new LocateControl({
+        position: "bottomright",
+      });
+
+      map.addControl(locateControl);
+
+      return () => {
+        map.removeControl(locateControl);
+      };
+    }
+  }, [map]);
+
+  return null;
+}
 
 export interface DynamicMapProps extends MapContainerProps {
   ref?: React.Ref<Leaflet.Map>;
@@ -24,6 +48,7 @@ export function DynamicMap(props: DynamicMapProps) {
   return (
     <MapContainer ref={ref} className="h-full w-full grow" {...rest}>
       {props.children}
+      <MapPlugins />
     </MapContainer>
   );
 }
