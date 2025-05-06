8e2de37a3b53168d465af2072711c2396d84a388
Constantin Müller <info@mueller-constantin.de>
2025-05-06 16:01:52 +0200

    feat: update manifest and favicons

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -37,6 +37,21 @@ export default async function RootLayout({
     <html lang={locale}>
       <head>
         <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Kippenstummel" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={`${lato.variable} bg-white dark:bg-slate-800`}>
         <NextIntlClientProvider>
