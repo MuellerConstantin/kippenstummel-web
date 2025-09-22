a8e013b4a08775bc9e6161d20ba47cafa77c3020
Constantin Müller <info@mueller-constantin.de>
2025-09-22 12:05:24 +0200

    feat: update pre-release banner style

--- src/components/organisms/PreReleaseBanner.tsx
+++ src/components/organisms/PreReleaseBanner.tsx
@@ -5,6 +5,7 @@ import { AnimatePresence } from "framer-motion";
 import { Link } from "@/components/atoms/Link";
 import { Modal } from "@/components/atoms/Modal";
 import { PreReleaseInfoDialog } from "./PreReleaseInfoDialog";
+import { TriangleAlert as TriangleAlertIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function PreReleaseBanner() {
@@ -16,21 +17,20 @@ export default function PreReleaseBanner() {
     <>
       <div className="bg-amber-600 text-white">
         <div className="flex items-center justify-center p-1">
-          <div className="text-center text-xs">
-            {t.rich("message", {
-              bold: (chunks) => (
-                <span className="font-bold">{chunks}&nbsp;</span>
-              ),
-              link: (chunks) => (
-                <Link
-                  variant="secondary"
-                  className="cursor-pointer"
-                  onPress={() => setShowInfoDialog(true)}
-                >
-                  {chunks}
-                </Link>
-              ),
-            })}
+          <div className="flex flex-wrap justify-center gap-1 text-center text-xs">
+            <TriangleAlertIcon className="h-4 w-4" />
+            <div>
+              {t.rich("message", {
+                bold: (chunks) => <span className="font-bold">{chunks}</span>,
+              })}
+            </div>
+            <Link
+              variant="secondary"
+              className="cursor-pointer"
+              onPress={() => setShowInfoDialog(true)}
+            >
+              {t("learnMore")}
+            </Link>
           </div>
         </div>
       </div>
@@ -41,7 +41,6 @@ export default function PreReleaseBanner() {
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0 }}
             transition={{ duration: 0.3, ease: "easeInOut" }}
-            className="!max-w-2xl"
             isOpen={showInfoDialog}
             onOpenChange={setShowInfoDialog}
           >
