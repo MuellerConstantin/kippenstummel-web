f21864f26e5e37c823021e9c0c852ec552e0c088
Constantin Müller <info@mueller-constantin.de>
2025-10-17 18:37:08 +0200

    feat: restore old avatars

--- src/components/atoms/IdentIcon.tsx
+++ src/components/atoms/IdentIcon.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { createAvatar } from "@dicebear/core";
-import { thumbs } from "@dicebear/collection";
+import { useEffect, useRef } from "react";
+import * as jdenticon from "jdenticon";
 
 interface IdentIconProps {
   value: string;
@@ -10,28 +9,21 @@ interface IdentIconProps {
 }
 
 export function IdentIcon({ value, className }: IdentIconProps) {
-  const [svg, setSvg] = useState<string>("");
+  const icon = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
-    const avatar = createAvatar(thumbs, {
-      seed: value,
-      backgroundColor: ["84cc16"],
-      shapeColor: ["16a34a"],
-      scale: 75,
-    });
-
-    let svgString = avatar.toString();
-    svgString = svgString
-      .replace(/width="[^"]*"/, 'width="100%"')
-      .replace(/height="[^"]*"/, 'height="100%"');
-
-    setSvg(svgString);
+    if (icon.current) {
+      jdenticon.update(icon.current, value);
+    }
   }, [value]);
 
   return (
-    <div
-      className={`relative h-full w-full ${className || ""}`}
-      dangerouslySetInnerHTML={{ __html: svg }}
+    <svg
+      data-jdenticon-value={value}
+      ref={icon}
+      height="auto"
+      width="auto"
+      className={`relative h-full w-full bg-gray-100 dark:bg-gray-800 ${className}`}
     />
   );
 }
