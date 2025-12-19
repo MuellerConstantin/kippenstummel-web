6e16cfa1caef18508ceb64a47330336cb9be5fe8
Constantin Müller <info@mueller-constantin.de>
2025-12-19 16:40:00 +0100

    feat: extend cvm map view controller

--- src/contexts/CvmMapViewContext.tsx
+++ src/contexts/CvmMapViewContext.tsx
@@ -2,29 +2,44 @@ import { createContext, useCallback, useContext, useState } from "react";
 
 type CvmMapViewMode = "default" | "register" | "reposition";
 
-interface CvmMapViewState {
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface DefaultViewState {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface CvmMapRegisterViewState {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface CvmMapRepositionViewState {}
+
+export type CvmMapViewState =
+  | DefaultViewState
+  | CvmMapRegisterViewState
+  | CvmMapRepositionViewState;
+
+export interface CvmMapController {
   mode: CvmMapViewMode;
+  state: CvmMapViewState;
   changeMode: (mode: CvmMapViewMode) => void;
 }
 
-const cvmMapViewContext = createContext<CvmMapViewState | null>(null);
+const cvmMapViewContext = createContext<CvmMapController | null>(null);
 
 export function CvmMapViewProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [mode, setMode] = useState<CvmMapViewState["mode"]>("default");
+  const [mode, setMode] = useState<CvmMapController["mode"]>("default");
 
   const changeMode = useCallback(
-    (mode: CvmMapViewState["mode"]) => {
+    (mode: CvmMapController["mode"]) => {
       setMode(mode);
     },
     [setMode],
   );
 
   return (
-    <cvmMapViewContext.Provider value={{ mode, changeMode }}>
+    <cvmMapViewContext.Provider value={{ mode, changeMode, state: {} }}>
       {children}
     </cvmMapViewContext.Provider>
   );
@@ -38,4 +53,36 @@ export function useCvmMapView() {
   }
 
   return context;
+}
+
+export function useCvmMapDefaultView() {
+  const { mode } = useCvmMapView();
+
+  if (mode !== "default") {
+    throw new Error("useDefaultMapView can only be used in default view");
+  }
+
+  return {};
+}
+
+export function useCvmMapRegisterView() {
+  const { mode } = useCvmMapView();
+
+  if (mode !== "register") {
+    throw new Error("useCvmMapRegisterView can only be used in register view");
+  }
+
+  return {};
+}
+
+export function useCvmMapRepositionView() {
+  const { mode } = useCvmMapView();
+
+  if (mode !== "reposition") {
+    throw new Error(
+      "useCvmMapRepositionView can only be used in reposition view",
+    );
+  }
+
+  return {};
 }
