7e468af0d888f036e2e66613a1987260218e66aa
Constantin Müller <info@mueller-constantin.de>
2026-01-18 18:00:18 +0100

    feat: add custom error page

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -24,15 +24,15 @@ export default async function NotFound() {
     <StackTemplate>
       <div className="relative isolate flex grow items-center justify-center overflow-hidden px-4 py-12">
         <section>
-          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
-            <div className="flex max-w-screen-sm flex-col items-center">
-              <h1 className="mb-4 flex items-center gap-4 text-center text-7xl font-extrabold tracking-tight text-green-600 lg:text-9xl dark:text-green-600">
+          <div className="mx-auto max-w-screen px-4 py-8 lg:px-6 lg:py-16">
+            <div className="flex max-w-screen flex-col items-center gap-4">
+              <h1 className="flex items-center gap-4 text-center text-7xl font-extrabold tracking-tight text-green-600 lg:text-9xl dark:text-green-600">
                 <SearchXIcon className="h-16 w-16" />
               </h1>
-              <p className="mb-4 text-center text-3xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white">
+              <p className="text-center text-3xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white">
                 {t("headline")}
               </p>
-              <p className="mb-4 text-center text-lg font-light text-gray-500 dark:text-gray-400">
+              <p className="text-center text-lg font-light text-gray-500 dark:text-gray-400">
                 {t("description")}
               </p>
               <Link
