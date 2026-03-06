956b5a297b1bbd7143eef78a86c770f11b283acb
Constantin Müller <info@mueller-constantin.de>
2026-03-06 21:19:15 +0100

    feat: add p-limit dependency and implement concurrency control in use osm addresses hook

--- src/hooks/cvm/useOsmAddresses.tsx
+++ src/hooks/cvm/useOsmAddresses.tsx
@@ -2,6 +2,7 @@ import { GeoCoordinates } from "@/lib/types/geo";
 import { useCallback, useMemo } from "react";
 import useSWR from "swr";
 import axios from "axios";
+import pLimit from "p-limit";
 import { Cvm } from "@/lib/types/cvm";
 
 interface UseOsmAddressesProps {
@@ -45,6 +46,8 @@ type OsmAddress = {
 };
 
 export function useOsmAddresses({ cvms }: UseOsmAddressesProps) {
+  const limit = pLimit(3);
+
   const fetchOsmAddress = useCallback(async (key: GeoCoordinates) => {
     const url = "/api/geocoding/reverse";
 
@@ -75,7 +78,9 @@ export function useOsmAddresses({ cvms }: UseOsmAddressesProps) {
         ]
       : null,
     (key) =>
-      Promise.allSettled(key[1].map(fetchOsmAddress)).then((responses) =>
+      Promise.allSettled(
+        key[1].map((coords) => limit(() => fetchOsmAddress(coords))),
+      ).then((responses) =>
         responses.map((res) =>
           res.status === "fulfilled" ? res.value.data : null,
         ),
